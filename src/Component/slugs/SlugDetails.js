043967716f2036455a4b233ca8811c20d403daaa
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2024-12-23 17:04:23 +0530

    changessaed

---
+++ src/Component/slugs/SlugDetails.js
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import { API_CONFIG } from "@/config/app_config";
+const SlugDetails = ({ slugName }) => {
+    const [slug_data, setSlug_data] = useState();
+    console.log(slug_data, "this is the slug data")
+
+    const getData = async () => {
+        const response = await fetch(
+            `${API_CONFIG.BASE_URL}/get_page_by_slug?slug=${slugName}`
+        );
+
+        if (!response.ok) {
+            return null;
+        }
+
+        const data = await response.json();
+        if (data.type === "success") {
+            setSlug_data(data?.data)
+            return data.data;
+        }
+        return null;
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+    return (
+        <div>
+            <div className="cardBg p-2  text-center">
+                <Image
+                    src="/assets/phloii_logo.png"
+                    className="m-auto"
+                    alt="logo"
+                    width={100}
+                    height={59}
+                />
+            </div>
+            <div className="min-vh-100 bg-black  pt-5 pb-5">
+                <div
+                    className="container mx-auto text-white"
+                    dangerouslySetInnerHTML={{ __html: slug_data?.content || "" }}
+                ></div>
+            </div>
+        </div>
+    );
+};
+
+export default SlugDetails;
