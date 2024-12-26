a495bf1967e8cb520ce913b4c3f91422853b93e7
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2024-12-26 17:27:01 +0530

    changes saved

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,10 @@ const nextConfig = {
             const isProduction = process.env.VERCEL_ENV === "production";
             const isPreview = process.env.VERCEL_ENV === "preview";
             return isProduction
-                ? "https://services.phloii.com/api/v1/" // Production URL
+                ? "https://services.phloii.com/api/v1" // Production URL
                 : isPreview
-                    ? "https://dev.phloii.com/api/v1/" // Preview URL
-                    : "https://dev.phloii.com/api/v1/";    // Development URL
+                    ? "https://dev.phloii.com/api/v1" // Preview URL
+                    : "https://dev.phloii.com/api/v1";    // Development URL
         })(),
     },
 };
--- src/config/app_config.js
+++ src/config/app_config.js
@@ -2,9 +2,9 @@ export const API_CONFIG = {
     BASE_URL:
       process.env.NEXT_PUBLIC_API_URL ||
       (process.env.NODE_ENV === "production"
-        ? "https://services.phloii.com/api/v1/" // Production URL
+        ? "https://services.phloii.com/api/v1" // Production URL
         : process.env.VERCEL_ENV === "preview"
-        ? "https://dev.phloii.com/api/v1/" // Staging URL
-        : "https://dev.phloii.com/api/v1/"), // Development URL
+        ? "https://dev.phloii.com/api/v1" // Staging URL
+        : "https://dev.phloii.com/api/v1"), // Development URL
   };
   
