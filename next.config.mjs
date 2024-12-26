ec06bd10d0d21692dab484973453fdf8eac5d72c
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2024-12-26 17:21:16 +0530

    changes saved

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_API_URL: (() => {
+            const isProduction = process.env.VERCEL_ENV === "production";
+            const isPreview = process.env.VERCEL_ENV === "preview";
+            return isProduction
+                ? "https://services.phloii.com/api/v1/" // Production URL
+                : isPreview
+                    ? "https://dev.phloii.com/api/v1/" // Preview URL
+                    : "https://dev.phloii.com/api/v1/";    // Development URL
+        })(),
+    },
+};
 
 export default nextConfig;
