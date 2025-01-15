17cbb1a41f64793bde4e80008b15a9338c68aaac
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2025-01-15 19:45:24 +0530

    register button added

--- src/Component/homeComponent/HomeComponent.js
+++ src/Component/homeComponent/HomeComponent.js
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Testimonials from "@/Component/Testimonials";
-
+import Link from "next/link";
 export default function HomeComponent() {
   return (
     <div className={styles.homeWrapper}>
       <header className={styles.header}>
-        <Image src="/assets/logo.svg" width={139} height={57} alt="logo" />
+        <div className="container">
+
+       <div className="d-flex justify-content-between align-items-center">
+       <Image src="/assets/logo.svg" width={139} height={57} alt="logo" /> <Link href={'/establishment/login'} className={styles.rister_est}>Register Establishment</Link>
+       </div>
+        </div>
       </header>
       <div className={`${styles.mainBanner}`}>
         <div className="container position-relative">
@@ -468,6 +473,9 @@ export default function HomeComponent() {
                 offering a great opportunity to boost
                 sales while promoting safety for all users.
               </p>
+             <div className="pt-3">
+             <Link href={'/establishment/login'} className={styles.rister_est}>Register Establishment</Link>
+             </div>
             </div>
           </div>
         </div>
