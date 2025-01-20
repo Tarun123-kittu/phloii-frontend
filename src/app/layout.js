414c39c49a2614d158e291c54b663e093a1cb012
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2025-01-20 18:52:02 +0530

    latest build updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,14 +5,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import AppStore from "@/utils/redux/AppStore";
 import { Toaster } from "react-hot-toast";
-import Head from "next/head";
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-poppins",
   weight: "100 900",
 });
-
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -22,35 +19,7 @@ const geistMono = localFont({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        {/* General meta tags */}
-        <meta property="og:image" content="https://staging.phloii.com/assets/phloii_fav_icon.png" />
-        <meta property="og:image:secure_url" content="https://staging.phloii.com/assets/phloii_fav_icon.png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="628" />
-        
-        {/* Favicon */}
-        <link rel="icon" href="/black.svg" type="image/svg+xml" />
-        
-        {/* Facebook meta tags */}
-        <meta property="og:url" content="https://staging.phloii.com" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Phloii" />
-        <meta property="og:description" content="Find Matches Near you" />
-        
-        {/* Twitter meta tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourtwitterhandle" />
-        <meta name="twitter:title" content="Phloii" />
-        <meta name="twitter:description" content="Find Matches Near you" />
-        <meta name="twitter:image" content="https://staging.phloii.com/assets/phloii_fav_icon.png" />
-
-        {/* WhatsApp meta tag */}
-        <meta property="og:whatsapp" content="whatsapp://send?text=Check%20this%20out%20https://staging.phloii.com" />
-
-        {/* Skype meta tag */}
-        <meta name="skype_toolbar" content="skype:send?chat&url=https://staging.phloii.com" />
-      </Head>
+      <link rel="shortcut icon" href="/assets/phloii_fav_icon.png" type="image/png" />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Provider store={AppStore}>
           {children}
