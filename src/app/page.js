3e5dc2bcd5e76e89e89775d6a4394638eb1a363c
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2025-01-20 18:00:38 +0530

    latest build updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import HomeComponent from "@/Component/homeComponent/HomeComponent";
+import image from "next/image";
 
 export async function generateMetadata() {
   const metadata = {
     title: "Phloii Dating App",
     description: "Find Matches Near you",
-    image: "/phloii_fav_icon.png", 
+    image: "/phloii_fav_icon.png", // Static image for all routes
   };
 
   return {
@@ -15,15 +15,19 @@ export async function generateMetadata() {
       description: metadata.description,
       images: [
         {
-          url: "https://www.phloii.com"+metadata.image,
-          width: 1200, 
-          height: 630, 
+          url: `https://www.phloii.com${metadata.image}`, // Absolute path for OpenGraph image
+          width: 1200,
+          height: 630,
         },
       ],
+    },
+    icons: {
+      icon: metadata.image, // Set favicon
     },
   };
 }
 
-export default function Home() {
-  return <HomeComponent />;
+export default function Home({ params }) {
+  return <div>This is a dynamic page for {params.slug}</div>;
 }
+
