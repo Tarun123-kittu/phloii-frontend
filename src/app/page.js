eddc2fce21afde40a480795473d9edc207315a5a
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2025-01-21 10:52:27 +0530

    latest build

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,31 @@
 import HomeComponent from "@/Component/homeComponent/HomeComponent";
-import image from "next/image";
-
-export async function generateMetadata() {
-
-  const metadata = {
-    title: `Phloii app of dating`,
-    description: `You can find your match here`,
-    image: "https://images.unsplash.com/photo-1737320372090-c61d2cb88ab3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8",
-  };
-
-  return {
-    title: metadata.title,
-    description: metadata.description,
-    openGraph: {
-      title: metadata.title,
-      description: metadata.description,
-      images: [
-        {
-          url: `${metadata.image}`, 
-          width: 1200,
-          height: 630,
-        },
-      ],
-    },
-    icons: {
-      icon: metadata.image,
-    },
-  };
-}
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <HomeComponent />
+    <>
+      <Head>
+        <title>Phloii App of Dating</title>
+        <meta name="description" content="You can find your match here" />
+        <meta property="og:title" content="Phloii App of Dating" />
+        <meta property="og:description" content="You can find your match here" />
+        <meta
+          property="og:image"
+          content="https://images.unsplash.com/photo-1737320372090-c61d2cb88ab3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8"
+        />
+        <meta property="og:url" content="https://staging.phloii.com/" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Phloii App of Dating" />
+        <meta name="twitter:description" content="You can find your match here" />
+        <meta
+          name="twitter:image"
+          content="https://images.unsplash.com/photo-1737320372090-c61d2cb88ab3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8"
+        />
+      </Head>
+      <main>
+        <HomeComponent />
+      </main>
+    </>
   );
 }
+
