566cc08e1f8234b7a20a312185dcd96cca0339e5
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2025-01-21 10:41:27 +0530

    latest build

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,31 @@
 import HomeComponent from "@/Component/homeComponent/HomeComponent";
+import image from "next/image";
 
 export async function generateMetadata() {
 
   const metadata = {
-      title: `Phloii Dating App`,
-      description: `Find Matches Near you`,
+    title: `Phloii Dating App`,
+    description: `Find Matches Near you`,
+    image: "https://images.unsplash.com/photo-1737320372090-c61d2cb88ab3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8",
   };
 
   return {
+    title: metadata.title,
+    description: metadata.description,
+    openGraph: {
       title: metadata.title,
       description: metadata.description,
+      images: [
+        {
+          url: `${metadata.image}`, 
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+    icons: {
+      icon: metadata.image,
+    },
   };
 }
 
