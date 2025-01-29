afc0faddcbdb5ba68c327e90590fbf0724ee9407
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2025-01-29 14:08:54 +0530

    latest build changes

--- src/Component/Sidebar/SideBar.js
+++ src/Component/Sidebar/SideBar.js
@@ -24,7 +24,7 @@ const SideBar = ({ children }) => {
     router.push("/establishment/login");
   };
   const handleLogo = () => {
-    router.push("establishment");
+    router.push("/establishment");
   };
 
   useEffect(() => {
