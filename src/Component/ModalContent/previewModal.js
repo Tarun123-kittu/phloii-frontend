b0961330292643dd23ce09eac0c723c872a20e81
harmeetkaursohi95@gmail.com <harmeetkaursohi95@gmail.com>
2025-02-07 17:45:26 +0530

    latest build updated

--- src/Component/ModalContent/previewModal.js
+++ src/Component/ModalContent/previewModal.js
@@ -5,14 +5,22 @@ import CommonModal from "../Modal/commonModal";
 import "./modalContent.css";
 
 
-const PreviewModal = ({ show, onClose,hotel_details }) => {
+const PreviewModal = ({ show, onClose, hotel_details }) => {
+    console.log(hotel_details,"this is the hotel details")
 
     return (
         <CommonModal className={"eventModal"} show={show} onClose={onClose}>
             <div className="">
                 <div className="outer_events_fields">
                     <h2 className="main_heading text-left mt-2">Preview</h2>
-             
+                    <div className="d-flex gap-4">
+                        {hotel_details?.hotel?.images?.map((image,i) => {
+                            return(
+                                <img src={image} alt="support" width={70} height={70}/>
+                            )
+                        })}
+                       
+                    </div>
                 </div>
             </div>
         </CommonModal>
